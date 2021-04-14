6955bc16f92ae43893a23094983d7fb18a9e3b7e
Ihor Radchenko <yantar92@gmail.com>
2021-04-14 20:48:46 +0800

    Fix preamble for body in when and unless predicates

--- org-ql.el
+++ org-ql.el
@@ -1068,7 +1068,7 @@ predicates."
        ,@(mapcar #'org-ql-normalize-query clauses))))
   :preambles
   ((`(when ,condition . ,clauses)
-    (-let* (((&plist :regexp :case-fold :query) (org-ql-query-preamble `(and ,condition ,(last clauses)))))
+    (-let* (((&plist :regexp :case-fold :query) (org-ql-query-preamble `(and ,condition ,(car (last clauses))))))
       (list :regexp regexp
             :case-fold case-fold
             :query `(when ,condition ,@clauses))))))
@@ -1081,7 +1081,7 @@ predicates."
        ,@(mapcar #'org-ql-normalize-query clauses))))
   :preambles
   ((`(unless ,condition . ,clauses)
-    (-let* (((&plist :regexp :case-fold :query) (org-ql-query-preamble ,(last clauses))))
+    (-let* (((&plist :regexp :case-fold :query) (org-ql-query-preamble (car (last clauses)))))
       (list :regexp regexp
             :case-fold case-fold
             :query `(unless ,condition ,@clauses))))))
